53632c6cf49a7e0026c7a93f5b0f28e125bbaf17
HoangAnhTuan <hoanganhtuan95ptit@gmail.com>
2020-01-31 22:31:28 +0700

    format code và sửa lỗi upload phim

--- src/components/AddFilm.vue
+++ src/components/AddFilm.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="add-film">
+<div class="add-film">
     <input-search-film v-model="film.name" @choose="chooseFilm" />
     <div class="poster">
-      <img ref="posterImage" :src="film.poster">
-      <input ref="posterInput" @change="changeImage('poster', $event)" type="file">
+        <img ref="posterImage" :src="film.poster">
+        <input ref="posterInput" @change="changeImage('poster', $event)" type="file">
     </div>
     <div class="banner">
-      <img ref="bannerImage" :src="film.banner">
-      <input ref="bannerInput" @change="changeImage('banner', $event)" type="file">
+        <img ref="bannerImage" :src="film.banner">
+        <input ref="bannerInput" @change="changeImage('banner', $event)" type="file">
     </div>
     <table>
-      <tr>
-        <td>Tên</td>
-        <td><input v-model="film.name" type="text"></td>
-      </tr>
-      <tr>
-        <td>Tên tiếng anh</td>
-        <td><input v-model="film.nameEnglish" type="text"></td>
-      </tr>
-      <tr>
-        <td>Mô tả</td>
-        <td><textarea v-model="film.description" style="height: 200px" /></td>
+        <tr>
+            <td>Tên</td>
+            <td><input v-model="film.name" type="text"></td>
+        </tr>
+        <tr>
+            <td>Tên tiếng anh</td>
+            <td><input v-model="film.nameEnglish" type="text"></td>
+        </tr>
+        <tr>
+            <td>Mô tả</td>
+            <td><textarea v-model="film.description" style="height: 200px" /></td>
       </tr>
       <tr>
         <td>Diễn viên</td>
@@ -109,254 +109,305 @@ import InputSearchLink from './InputSearchLink'
 import axios from 'axios'
 
 export default {
-  name: 'AddFilm',
+    name: 'AddFilm',
 
-  components: { InputDropdown, InputSearchFilm, InputSearchLink },
-
-  data() {
-    return {
-      film: {
-        chapters: [],
-        banner: '',
-        poster: ''
-      },
-      casts: '',
-      directors: '',
-      releases: [],
-      nations: [],
-      genres: [],
-      qualities: [],
-      webs: []
-    }
-  },
-
-  mounted() {
-    this.getReleases()
-    this.getNations()
-    this.getGenres()
-    this.getQualities()
-    this.getWebs()
-  },
-
-  methods: {
-    addFilm() {
-      const film = { ...this.film }
-      const formData = new FormData()
-
-      film.casts = this.casts
-      film.directors = this.directors
-
-      const poster = this.$refs.posterInput.files[0] || this.film.poster
-      const banner = this.$refs.bannerInput.files[0] || this.film.banner
-
-      formData.append('film', film)
-      formData.append('poster', poster)
-      formData.append('banner', banner)
-
-      axios.post('http://45.76.145.91:8080/apilink/admin/film/insertOrUpdate', formData)
+    components: {
+        InputDropdown,
+        InputSearchFilm,
+        InputSearchLink
     },
 
-    changeImage(type, $event) {
-      const files = $event.target.files
-
-      if (files[0]) {
-        var reader = new FileReader()
-
-        reader.onload = (e) => {
-          this.film[type] = e.target.result
-        }
-
-        reader.readAsDataURL(files[0])
-      }
-    },
-
-    addChapter() {
-      const chapters = this.film.chapters || []
-      chapters.unshift({
-        name: '',
-        duration: '',
-        links: [
-          {
-            source: '',
+    data() {
+        return {
+            film: {
+                chapters: [],
+                banner: '',
+                poster: ''
+            },
+            casts: '',
+            directors: '',
+            releases: [],
+            nations: [],
+            genres: [],
             qualities: [],
-            web: []
-          }
-        ]
-      })
-
-      this.film.chapters = []
-
-      this.$nextTick(() => {
-        this.film.chapters = chapters
-      })
+            webs: []
+        }
     },
 
-    deleteChapter(index) {
-      const chapters = this.film.chapters || []
-
-      chapters.splice(index, 1)
-
-      this.$set(this.film.chapters, chapters)
+    mounted() {
+        this.getReleases()
+        this.getNations()
+        this.getGenres()
+        this.getQualities()
+        this.getWebs()
     },
 
-    addLink(index) {
-      const links = this.film.chapters[index].links || []
+    methods: {
+        addFilm() {
+            const film = {
+                ...this.film
+            }
+            const formData = new FormData()
 
-      links.unshift({
-        source: '',
-        qualities: [],
-        web: []
-      })
+            film.casts = this.convertStringToListTopic(this.casts, 1)
+            film.directors = this.convertStringToListTopic(this.directors, 2)
 
-      this.film.chapters[index].links = []
+            const poster = this.$refs.posterInput.files[0] || this.film.poster
+            const banner = this.$refs.bannerInput.files[0] || this.film.banner
 
-      this.$nextTick(() => {
-        this.film.chapters[index].links = links
-      })
-    },
+            formData.append('film', JSON.stringify(film))
+            formData.append('poster', poster)
+            formData.append('banner', banner)
 
-    deleteLink(chapter, index) {
-      const links = chapter.links || []
+            axios.post('http://45.76.145.91:8080/apilink/admin/film/insertOrUpdate', formData)
+        },
 
-      links.splice(index, 1)
+        /**
+         * chuyển đổi từ string sang topic
+         * các name topic cách nhau bằng dấu phẩy trong text
+         * phục vụ cho diễn viên và đạo diễn
+         * 
+         * @text đây là dữ liệu cần tách để tạo ra các topic
+         * @typeTopic type của topic, type có thể có các giá trị sau: 
+         *    SYSTEM(0, "Hệ thống"),
+         *    CAST(1, "Diễn viên"),
+         *    DIRECTOR(2, "Đạo diễn"),
+         *    RELEASE(3, "Năm phát hành"),
+         *    TAG(4, "Tags"),
+         *    QUALITY(5, "Chất lượng phim"),
+         *    NATION(6, "Quốc gia"),
+         *    GENRE(7, "Thể loại phim"),
+         *    WEB(8, "Web phim")
+         */
+        convertStringToListTopic(text, typeTopic) {
+            const listStr = text.split(", ") // lấy tên của các topic
+            const listTopic = Array(listStr.length)
 
-      this.$set(chapter.links, links)
-    },
+            for (let i = 0; i < listStr.length; i += 1) {
+                const topic = { // khởi tạo topic
+                    name: '',
+                    type: typeTopic
+                }
+                topic.name = listStr[i]
+                listTopic[i] = topic
+            }
 
-    chooseFilm(film) {
-      this.film = film
+            return listTopic
+        },
 
-      this.casts = (this.film.casts || []).map((cast) => cast.name).join(', ')
-      this.directors = (this.film.directors || []).map((director) => director.name).join(', ')
-    },
+        changeImage(type, $event) {
+            const files = $event.target.files
 
-    chooseLink(link, event) {
-      link.source = event.source
-      link.qualities = event.qualities
-      link.webs = event.webs
-    },
+            if (files[0]) {
+                var reader = new FileReader()
 
-    async getReleases() {
-      const { data } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=3')
-      this.releases = data.data.topics
-    },
+                reader.onload = (e) => {
+                    this.film[type] = e.target.result
+                }
 
-    async getNations() {
-      const { data } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=6')
-      this.nations = data.data.topics
-    },
+                reader.readAsDataURL(files[0])
+            }
+        },
 
-    async getGenres() {
-      const { data } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=7')
-      this.genres = data.data.topics
-    },
+        addChapter() {
+            const chapters = this.film.chapters || []
+            chapters.unshift({
+                name: '',
+                duration: '',
+                links: [{
+                    source: '',
+                    qualities: [],
+                    web: []
+                }]
+            })
 
-    async getQualities() {
-      const { data } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=5')
-      this.qualities = data.data.topics
-    },
+            this.film.chapters = []
 
-    async getWebs() {
-      const { data } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=8')
-      this.webs = data.data.topics
+            this.$nextTick(() => {
+                this.film.chapters = chapters
+            })
+        },
+
+        deleteChapter(index) {
+            const chapters = this.film.chapters || []
+
+            chapters.splice(index, 1)
+
+            this.$set(this.film.chapters, chapters)
+        },
+
+        addLink(index) {
+            const links = this.film.chapters[index].links || []
+
+            links.unshift({
+                source: '',
+                qualities: [],
+                web: []
+            })
+
+            this.film.chapters[index].links = []
+
+            this.$nextTick(() => {
+                this.film.chapters[index].links = links
+            })
+        },
+
+        deleteLink(chapter, index) {
+            const links = chapter.links || []
+
+            links.splice(index, 1)
+
+            this.$set(chapter.links, links)
+        },
+
+        chooseFilm(film) {
+            this.film = film
+
+            this.casts = (this.film.casts || []).map((cast) => cast.name).join(', ')
+            this.directors = (this.film.directors || []).map((director) => director.name).join(', ')
+        },
+
+        chooseLink(link, event) {
+            link.source = event.source
+            link.qualities = event.qualities
+            link.webs = event.webs
+        },
+
+        async getReleases() {
+            const {
+                data
+            } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=3')
+            this.releases = data.data.topics
+        },
+
+        async getNations() {
+            const {
+                data
+            } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=6')
+            this.nations = data.data.topics
+        },
+
+        async getGenres() {
+            const {
+                data
+            } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=7')
+            this.genres = data.data.topics
+        },
+
+        async getQualities() {
+            const {
+                data
+            } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=5')
+            this.qualities = data.data.topics
+        },
+
+        async getWebs() {
+            const {
+                data
+            } = await axios.get('http://45.76.145.91:8080/apilink/admin/topic/list?type=8')
+            this.webs = data.data.topics
+        }
     }
-  }
 }
 </script>
 
 <style lang="scss">
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: Arial, Helvetica, sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
 }
 
 input[type="file"] {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  opacity: 0;
-  top: 0;
-  left: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    top: 0;
+    left: 0;
 }
 
 .poster {
-  margin-top: 12px;
-  width: 100%;
-  height: 300px;
-  position: relative;
-  img {
+    margin-top: 12px;
     width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: top;
-  }
+    height: 300px;
+    position: relative;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: top;
+    }
 }
 
 .banner {
-  margin: auto;
-  width: 200px;
-  height: 200px;
-  margin-top: -100px;
-  position: relative;
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: top;
-  }
+    margin: auto;
+    width: 200px;
+    height: 200px;
+    margin-top: -100px;
+    position: relative;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: top;
+    }
 }
 
 .table {
-  padding: 8px;
-  border: 1px solid #ccc;
+    padding: 8px;
+    border: 1px solid #ccc;
 }
 
 .table-header {
-  padding: 16px;
+    padding: 16px;
 }
 
 .table-link {
-  background: #eee;
-  border-collapse: collapse;
-  border-spacing: 0;
-  td {
-    padding: 8px;
-  }
+    background: #eee;
+    border-collapse: collapse;
+    border-spacing: 0;
+
+    td {
+        padding: 8px;
+    }
 }
 
 .add-film {
-  width: 70%;
-  margin: auto;
+    width: 70%;
+    margin: auto;
 }
 
 button {
-  border: none;
-  outline: none;
-  padding: 8px 16px;
-  &:hover {
-    cursor: pointer;
-    opacity: 0.7;
-  }
+    border: none;
+    outline: none;
+    padding: 8px 16px;
+
+    &:hover {
+        cursor: pointer;
+        opacity: 0.7;
+    }
 }
 
 table {
-  width: 90%;
+    width: 90%;
 }
 
 input,
 textarea {
-  width: 100%;
-  height: 32px;
-  border-radius: 4px;
-  border: 1px solid #ccc;
-  outline: none;
-  padding: 0 8px;
+    width: 100%;
+    height: 32px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    outline: none;
+    padding: 0 8px;
 }
 
 textarea {
-  height: 64px;
-  padding: 8px;
+    height: 64px;
+    padding: 8px;
 }
 </style>
